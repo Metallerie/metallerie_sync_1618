04ed62fab6228374a46b39601d1ba7dce221a0b7
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-01-01 23:05:39 +0100

    Update sync_manager.py

--- models/sync_manager.py
+++ models/sync_manager.py
@@ -1,11 +1,25 @@
-
-from .sync_company import SyncCompany
-from .sync_products import SyncProducts
-from .sync_partners import SyncPartners
+import psycopg2
 
 class SyncManager:
     @staticmethod
+    def _get_connection(dbname):
+        """
+        Récupère une connexion à la base spécifiée.
+        """
+        return psycopg2.connect(
+            dbname=dbname,
+            user='odoo',
+            password='0625159120',
+            host='localhost',
+            port='5432'
+        )
+
+    @staticmethod
     def run_global_sync():
-        SyncCompany.sync()
-        SyncProducts.sync()
-        SyncPartners.sync()
+        """
+        Orchestrateur de synchronisation pour tous les modèles.
+        """
+        from .sync_company import SyncCompany
+        SyncCompany.sync_v16_to_v18()
+        # Ajouter d'autres appels à des synchronisations ici (e.g., produits, partenaires, etc.)
+        print("Synchronisation globale terminée.")

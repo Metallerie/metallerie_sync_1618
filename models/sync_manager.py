81158c30a94d0076f7bb96806e4d88ee4a4aab09
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-01-01 22:46:32 +0100

    Update sync_manager.py

--- models/sync_manager.py
+++ models/sync_manager.py
@@ -1,1 +1,11 @@
 
+from .sync_company import SyncCompany
+from .sync_products import SyncProducts
+from .sync_partners import SyncPartners
+
+class SyncManager:
+    @staticmethod
+    def run_global_sync():
+        SyncCompany.sync()
+        SyncProducts.sync()
+        SyncPartners.sync()

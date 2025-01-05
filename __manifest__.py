b77e90d69f3048f2ec491255c7dace9a70eb290c
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-01-05 17:15:09 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -34,7 +34,7 @@ metallerie_sync_1618/
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','partners','product','sale_management','website','stock','purchase','website_sale','mrp','project_todo'],
+    'depends': ['base','account','product','sale_management','website','stock','purchase','website_sale','mrp','project_todo'],
 
     # always loaded
     'data': [

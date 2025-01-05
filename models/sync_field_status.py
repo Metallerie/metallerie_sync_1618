4216f8abe861c2c362910a10170998c99b145a3f
Metallerie <98831730+Metallerie@users.noreply.github.com>
2025-01-05 22:40:36 +0100

    Update sync_field_status.py

--- models/sync_field_status.py
+++ models/sync_field_status.py
@@ -1,1 +1,10 @@
+class SyncFieldStatus(models.Model):
+    _name = 'metallerie.sync.field_status'
+    _description = 'Statut de synchronisation des champs'
+
+    field_name = fields.Char(string="Nom du champ")
+    field_type = fields.Char(string="Type du champ")
+    field_relation = fields.Char(string="Relation")
+    field_status = fields.Selection([('synced', 'Synchronisé'), ('ignored', 'Ignoré')], string="Statut")
+    ignore_reason = fields.Text(string="Raison ignorée")
 
